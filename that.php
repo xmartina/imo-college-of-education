7480c1e1c30ffc9eab88629338e2d7692e2f3a74
unknown <martinsprince126@gmail.com>
2023-02-08 11:40:05 +0100

    init push

---
+++ that.php
@@ -0,0 +1,33 @@
+<ul class="mainmenu">
+    <li class="has-droupdown">
+        <a class="nav-link" href="#">About Us</a>
+        <ul class="submenu">
+            <li><a href="https://imsced.edu.ng/students/ourhistory.html">History</a></li>
+            <li><a href="https://imsced.edu.ng/students/aboutus.html">Mission/Vision</a></li>
+            <li><a href="https://imsced.edu.ng/students/accreditation.html">Accreditation</a></li>
+        </ul>
+    </li>
+    <li class="has-droupdown">
+        <a class="nav-link" href="#">Academics</a>
+        <ul class="submenu">
+            <li><a href="https://imsced.edu.ng/students/schools.html">Schools</a></li>
+            <li><a href="https://imsced.edu.ng/students/howtopayfees.html">Payment Procedures</a></li>
+        </ul>
+    </li>
+    <li class="has-droupdown">
+        <a class="nav-link" href="#">Admission</a>
+        <ul class="submenu">
+            <li><a href="https://imsced.edu.ng/students/admission.html">Admission Requirements</a></li>
+            <li><a href="https://imsced.edu.ng/students/allcourses.html">A-Z Courses</a></li>
+            <li><a href="https://imsced.edu.ng/students/howtoapply.html">How to Apply</a></li>
+        </ul>
+    </li>
+    <li class="has-droupdown">
+        <a class="nav-link" href="#">Campus Life</a>
+        <ul class="submenu">
+            <li><a href="https://imsced.edu.ng/students/studentaffairs.html">Student Affairs</a></li>
+            <li><a href="https://imsced.edu.ng/students/hostel.html">Hostel</a></li>
+        </ul>
+    </li>
+    <li><a class="nav-item" href="https://imsced.edu.ng/students/contactus.html">Contact Us</a></li>
+</ul>
